c76f1125403d0aa5d36d89183e87b9bc002db9c9
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-09-08 11:53:03 -0700

    Update pca.py

--- sklearn/pca.py
+++ sklearn/pca.py
@@ -1,18 +1,25 @@
-Select dimension
+PCA is an unsupervised machine learning
+  It depends only upon the feature set 
+     does not depend the label/target data.
+Data preparation
+  PCA performs best with a normalized feature set
+  Do standard scalar normalization to normalize feature set
+
+PCA explained_variance_ratio_
   pca.explained_variance_ratio_
-  pca.explained_variance_ratio_ parameter returns 
-    a vector of the variance from each dimension. 
+    A vector of the variance from each dimension. 
 
-  pca.explained_variance_ratio_.cumsum() return 
-  a vector x of cumulative variance 
+  pca.explained_variance_ratio_.cumsum() 
+    A vector x of cumulative variance 
 
-
+Load library
   import numpy as np
   from sklearn.decomposition import PCA
   import matplotlib
   matplotlib.use('Agg') # agg
   import matplotlib.pyplot as plt # keep order
 
+Sample with random data
   np.random.seed(0)
   my_matrix = np.random.randn(20, 6)
 
@@ -29,3 +36,6 @@ Select dimension
   plt.ylabel('cumulative explained variance');
 
   plt.savefig('myfig')
+
+
+
