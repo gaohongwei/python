33197b449282af80945366bc0f9bfdd7b7d7a2b4
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-09-09 15:10:59 -0700

    Update pca.py

--- sklearn/try/pca.py
+++ sklearn/try/pca.py
@@ -12,30 +12,47 @@ PCA explained_variance_ratio_
   pca.explained_variance_ratio_.cumsum() 
     A vector x of cumulative variance 
 
-Load library
+def select_pca_by_variance(pct_sum):
+  comp_len = len(pct_sum)
+  index = 0
+  last_sum = 0.0
+  while index < comp_len:
+    current_sum = pct_sum[index]
+    current_pct = current_sum - last_sum
+    last_sum = current_sum
+    if ( current_sum > 0.95 and current_pct < 0.01 ) or \
+       ( current_sum > 0.90 and current_pct < 0.02):
+      break
+    else:
+      index += 1
+  return index
+
+def find_pca_count(X):
+  from sklearn.preprocessing import StandardScaler
+  from sklearn.decomposition import PCA  
+  sc = StandardScaler()
+  Xpca = sc.fit_transform(X)
+  pca = PCA()
+  pca.fit(Xpca)
+  return select_pca_by_variance(pca.explained_variance_ratio_.cumsum())
+
+def try_pca():
+  #Load library
   import numpy as np
   from sklearn.decomposition import PCA
   import matplotlib
   matplotlib.use('Agg') # agg
   import matplotlib.pyplot as plt # keep order
-
-Sample with random data
+  #Sample with random data
   np.random.seed(0)
   my_matrix = np.random.randn(20, 6)
-
   # pca = PCA().fit(my_matrix)
   pca = PCA()
   pca.fit(my_matrix)
-
   pca.explained_variance_
   pca.explained_variance_ratio_
   pca.explained_variance_ratio_.cumsum()
-
   plt.plot(np.cumsum(pca.explained_variance_ratio_))
   plt.xlabel('number of components')
   plt.ylabel('cumulative explained variance');
-
   plt.savefig('myfig')
-
-
-
