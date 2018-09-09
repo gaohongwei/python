e082811bcdd986beec3afd3f611f988e08d5650c
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-09-09 14:19:04 -0700

    Update pipeline.py

--- sklearn/try/pipeline.py
+++ sklearn/try/pipeline.py
@@ -1,4 +1,26 @@
+# Create as common method
+def create_pipeline(model):
+  from sklearn.pipeline import Pipeline
+  from sklearn.decomposition import PCA
+  from sklearn.preprocessing import StandardScaler
+  pipe = Pipeline([
+    ('sc', StandardScaler()),
+    ('pca', PCA()),
+    ('model',model)
+    ])
+  return pipe
 def try_pipeline():
+  from sklearn.svm import SVC  
+  from sklearn.datasets import load_digits  
+  digits = load_digits()
+  X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.20, random_state=0)
+  model = SVC()
+  pipe = create_pipeline(model)
+  pipe.fit(X_train,y_train)
+  print(pipe.score(X_test,y_test))
+
+
+def try_pipeline0():
   from sklearn.pipeline import Pipeline
   from sklearn.svm import SVC
   from sklearn.decomposition import PCA
@@ -16,4 +38,4 @@ def try_pipeline():
   pipe.fit(X_train,y_train)
   print(pipe.score(X_test,y_test))
 
-try_pipeline()
+try_pipeline0()
