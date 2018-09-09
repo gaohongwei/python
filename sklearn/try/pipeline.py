baf59436305ba76af96ca5ca6e2cb8f5dd684c15
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-09-09 15:24:37 -0700

    Update pipeline.py

--- sklearn/try/pipeline.py
+++ sklearn/try/pipeline.py
@@ -1,4 +1,28 @@
 # Create as common method
+def select_pca_by_variance(pct_sum):
+  comp_len = len(pct_sum)
+  index = 0
+  last_sum = 0.0
+  while index < comp_len:
+    current_sum = pct_sum[index]
+    current_pct = current_sum - last_sum
+    last_sum = current_sum
+    if ( current_sum > 0.95 and current_pct < 0.01 ) or \
+       ( current_sum > 0.90 and current_pct < 0.02):
+      break
+    else:
+      index += 1
+  return index
+
+def find_pca_count(X):
+  from sklearn.preprocessing import StandardScaler
+  from sklearn.decomposition import PCA  
+  sc = StandardScaler()
+  Xpca = sc.fit_transform(X)
+  pca = PCA()
+  pca.fit(Xpca)
+  return select_pca_by_variance(pca.explained_variance_ratio_.cumsum())
+
 def create_pipeline(model, pca_count):
   from sklearn.pipeline import Pipeline
   from sklearn.decomposition import PCA
@@ -10,16 +34,17 @@ def create_pipeline(model, pca_count):
     ])
   return pipe
 
-def try_pipeline(pca_count):
-  from sklearn.svm import SVC  
-  from sklearn.datasets import load_digits  
+def try_pipeline():
+  from sklearn.svm import SVC
+  from sklearn.datasets import load_digits
   digits = load_digits()
+  from sklearn.model_selection import train_test_split  
   X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.20, random_state=0)
   model = SVC()
+  pca_count = find_pca_count(X_train)
   pipe = create_pipeline(model, pca_count)
   pipe.fit(X_train,y_train)
   print(pipe.score(X_test,y_test))
-
 
 def try_pipeline0():
   from sklearn.pipeline import Pipeline
