d951f5b1c6add0c7ca70d99bf97623f87d0865cd
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-12-12 20:10:28 -0800

    Update and rename vent_strings.py to vect_strings.py

--- sklearn/text/vect_strings.py
+++ sklearn/text/vect_strings.py
@@ -1,4 +1,9 @@
 # Vectorizer
+# both can take list of filename
+     vectorizer = CountVectorizer(input='filename')
+     vectorizer = TfidfVectorizer(input='filename')
+# Or take list of strings
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 corpus = [
      'This is the first document.',
@@ -6,11 +11,14 @@ corpus = [
      'And this is the third one.',
      'Is this the first document?',
  ]
-vectorizer = CountVectorizer()
+#ectorizer = CountVectorizer()
+vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(corpus)
 print(vectorizer.get_feature_names())
 # ['and', 'document', 'first', 'is', 'one', 'second', 'the', 'third', 'this']
 print(X.toarray())  
+# array of count for CountVectorizer
+# array for float for TfidfVectorizer
 # [[0 1 1 1 0 0 1 0 1]
 #  [0 2 0 1 0 1 1 0 1]
 #  [1 0 0 1 1 0 1 1 1]
