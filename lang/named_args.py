240cb1f7a61d9e1d5a4fa7687dc93df55164de2b
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-07-13 10:38:32 -0700

    Update named_args.py

--- lang/named_args.py
+++ lang/named_args.py
@@ -2,19 +2,21 @@
 def m2(a='aaa', b=2):
   return a*b
 if __name__ == '__main__':
-  params1 = {
+  p1 = {
     'a': 10,
     'b': 2
   }
-  params2 = {
+  p2 = {
     'a': 10,
     'b': 20
   }
-  params3 = {
+  p3 = {
     'a': 'abc',
     'b': 3
   }
+  p4 = {}
   print(m2(a=10,b=20))
-  print(m2(**params1))
-  print(m2(**params2))
-  print(m2(**params3))
+  print(m2(**p1))
+  print(m2(**p2))
+  print(m2(**p3))
+  print(m2(**p4))
