e9ffcbd7caa74f9e153f37838d5bf65babf2b1ea
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-10-31 09:57:57 -0700

    Update loop.py

--- lang/loop.py
+++ lang/loop.py
@@ -1,2 +1,9 @@
->>> for key, value in a_dict.items():
-...     print(key, '->', value)
+Loop on a dict
+  for key, value in a_dict.items():
+    print(key, '->', value)
+  In Py2.x
+      dict.items(): Return a copy of the dictionary’s list of (key, value) pairs.
+      dict.iteritems(): Return an iterator over the dictionary’s (key, value) pairs.
+  In Py3.x, 
+      dict.items(), dict.keys() and dict.values()
+      Returns iterators
